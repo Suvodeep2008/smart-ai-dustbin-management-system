658a2caf52f8f8a6c912853aeb2197948af299d2
Suvodeep2008 <suvodeepdas33@gmail.com>
2026-09-15 01:14:37 +0530

    Fix Render CORS connection

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     "http://127.0.0.1:5501",
     "http://localhost:5501",
     "https://smart-ai-dustbin-management-system-1.onrender.com",
-    ],
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
