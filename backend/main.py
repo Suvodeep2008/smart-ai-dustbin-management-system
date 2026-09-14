86108db396a362080bf0bd665a5f06e19ccda92d
Suvodeep2008 <suvodeepdas33@gmail.com>
2026-09-15 01:13:09 +0530

    Fix Render CORS connection

--- backend/main.py
+++ backend/main.py
@@ -22,10 +22,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:5501",
-        "http://localhost:5501",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:5501",
+    "http://localhost:5501",
+    "https://smart-ai-dustbin-management-system-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
